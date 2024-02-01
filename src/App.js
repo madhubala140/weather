5532a92fa6e339222c09897cb70d7c999ce977c6
madhubala140 <madhusekar.s1994@gmail.com>
2024-02-01 22:02:09 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import Weather from './components/Weather';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
